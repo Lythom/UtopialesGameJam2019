4cef32e2c1cd784f80f36c941b91fec83550282b
Clément HUMBLOT <clement.humblot@epitech.eu>
2019-10-05 19:16:08 +0200

    [+Mutators]BreakWall triggers and mutator logic

---
+++ Assets/CorePhysics/MutaTrigger/MutActivator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class MutActivator : MonoBehaviour
+{
+    private bool activated = false;
+    public GameObject despawnEffect;
+    
+    public float leavingSpeed = 0.002f;
+    private static bool hasToMove = false;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GetComponent<Light>().intensity = 2;
+        if (other.CompareTag("Player"))
+        {
+            ChangeActivationStatus(other.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (hasToMove)
+        {
+            MoveAway();
+        }
+        if (transform.position.z <= -2.0f)
+            Destroy(gameObject);
+    }
+
+    public void SetMoving()
+    {
+        hasToMove = true;
+        Instantiate(despawnEffect, transform.position, transform.rotation);
+    }
+
+    void MoveAway()
+    {
+        transform.Translate(0, 0, -leavingSpeed);
+    }
+
+    void ChangeActivationStatus(GameObject player)
+    {
+        //Visual effect
+        activated = !activated;
+        GetComponent<Light>().intensity = activated ? 2 : 0;
+        //Update MutatorTriggers
+        player.GetComponent<MutablePlayer>().UpdateBonuses( player.GetComponent<MutablePlayer>().WALL_BREAKER_TYPE, activated ? 1 : -1);
+    }
+}
