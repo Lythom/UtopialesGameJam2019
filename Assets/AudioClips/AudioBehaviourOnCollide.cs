32d06a7d9ac8d1360a041880ec47bd63ced46032
zerogium <17790053+Zerogium@users.noreply.github.com>
2019-10-06 15:53:30 +0200

    Son Algues Plateau Kicker

---
+++ Assets/AudioClips/AudioBehaviourOnCollide.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioBehaviourOnCollide : MonoBehaviour
+{
+    private AudioSource launchHookAudioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        launchHookAudioSource = GetComponent<AudioSource>();
+        if (launchHookAudioSource == null) throw new Exception(" audio source is required");
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D()
+    {
+        launchHookAudioSource.Play();
+    }
+}
