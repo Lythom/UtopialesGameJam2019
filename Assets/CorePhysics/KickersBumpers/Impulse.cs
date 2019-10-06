6e7da452f058e7cb45f558e3c4763c2eadddaf74
zerogium <17790053+Zerogium@users.noreply.github.com>
2019-10-06 10:11:52 +0200

    Bumpers prefabs

--- Assets/CorePhysics/KickersBumpers/Impulse.cs
+++ Assets/CorePhysics/KickersBumpers/Impulse.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Impulse : MonoBehaviour
 {
-    public long impulsion; // 10 was good
+    public long impulsion = 10; // 10 was good
     private Rigidbody2D rb;
 
     void OnCollisionEnter2D(Collision2D other)
