78b961e344bd0c312a51df4ff9382af2ec71d30d
zerogium <17790053+Zerogium@users.noreply.github.com>
2019-10-20 01:27:32 +0200

    Add Baleine Prefab

---
+++ Assets/Teleport.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    public string targetSceneName;
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(targetSceneName);
+    }
+}
